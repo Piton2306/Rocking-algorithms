71a31ac40def308e2dc8d5079a096b953303bc23
Nastenko Peter <source-2306@mail.ru>
2021-04-04 22:03:29 +0300

    rename Sort => Speed_sort end

--- Speed_sort.py
+++ Speed_sort.py
@@ -5,8 +5,23 @@ def quick_sort(array):
         pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i > pivot]
-        return quick_sort(less) +[pivot] + quick_sort(greater)
+        return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
-x = [2, 4, 6, 657, 2]
-print(quick_sort(x))
+x = [2, 4, 6, 9, 2]
+
+
+def print_items(list):
+    for items in list:
+        print(items)
+
+
+from time import sleep
+
+
+def print_item_sleep(list):
+    for items in list:
+        sleep(1)
+
+        print(items,end=',')
+print_item_sleep(x)
